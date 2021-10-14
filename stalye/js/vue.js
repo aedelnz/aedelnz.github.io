1d1d9b41b34405ccc547f57c091c1365215c00f8
aedelnz <aedelnz@qq.com>
2021-10-14 16:23:04 +0800

    00

--- stalye/js/vue.js
+++
@@ -1,7 +0,0 @@
-//vue
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: '<a class="mdui-list-item mdui-list-item-content"  href="../A2zml">导航</a><a class="mdui-list-item mdui-list-item-content"  href="https://jixiejidiguan.baklib-free.com/">博客</a><a class="mdui-list-item mdui-list-item-content"  href="../xiangmu.html">项目</a><a class="mdui-list-item mdui-list-item-content"  href="../tuandui.html">关于</a>'
-  }
-})
--- stalye/js/zhucx.js
+++ stalye/js/zhucx.js
@@ -14,9 +14,9 @@ document.addEventListener('visibilitychange', function() {
 });
 
 //Log
-console.log ("%cJIXIEJIDIGUAN"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em");
-console.log('%c http://jixiejidiguan.top','font-size:35px;color:#ff8400;');
-console.log('%c ','background-image:url("http://jixiejidiguan.top/stalye/img/google.gif");background-size:120% 120%;background-repeat:no-repeat;background-position:center center;line-height:60px;padding:30px 120px;font-size:36px;color:red;');
+console.log ("%c JIXIEJIDIGUAN"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em");
+console.log('%c 地址 http://jixiejidiguan.top','font-size:35px;color:#ff8400;');
+console.log('%c ','background-image:url("https://jixiejidiguan.top/stalye/img/google.gif");background-size:100% 100%;background-repeat:no-repeat;background-position:center center;line-height:100px;padding:100px 180px;font-size:46px;color:red;');
 
 //友盟
 document.write(unescape("%3Cspan id='cnzz_stat_icon_1279931368'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1279931368%26online%3D1%26show%3Dline' type='text/javascript'%3E%3C/script%3E"));
