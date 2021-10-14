12bcdb4cdf7462f7db3c0c86870c92bd036d54b4
aedelnz <aedelnz@qq.com>
2021-10-14 14:41:23 +0800

    00

--- stalye/js/zhucx.js
+++ stalye/js/zhucx.js
@@ -14,14 +14,16 @@ document.addEventListener('visibilitychange', function() {
 });
 
 //Log
-console.log('什么都没, ------------------------\n ---------------------\n ------------------------------------------------------------------------\n ------------------------');
+console.log ("%cJIXIEJIDIGUAN"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em");
+console.log('%c http://jixiejidiguan.top','font-size:35px;color:#ff8400;');
+console.log('%c ','background-image:url("http://jixiejidiguan.top/stalye/img/google.gif");background-size:120% 120%;background-repeat:no-repeat;background-position:center center;line-height:60px;padding:30px 120px;font-size:36px;color:red;');
 
-//统计
 //友盟
-var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");
 document.write(unescape("%3Cspan id='cnzz_stat_icon_1279931368'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1279931368%26online%3D1%26show%3Dline' type='text/javascript'%3E%3C/script%3E"));
+
 //我要啦
 document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"JMIJZqK4MvC0pOsF\",ck:\"JMIJZqK4MvC0pOsF\"});</script>")
+
 //百度统计
 var _hmt = _hmt || [];
 (function() {
