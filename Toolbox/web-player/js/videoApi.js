8620e5965adfbdf7d92e2e71eed76f09142dfdf8
aedelnz <aedelnz@qq.com>
2021-12-12 12:31:34 +0800

    删库跑路

--- Toolbox/web-player/js/videoApi.js
+++
@@ -1,8 +0,0 @@
-api=[
-"https://jx.renrenmi.cc/?url=",
-"https://jiexi.t7g.cn/?url=",
-"https://api.leduotv.com/wp-api/ifr.php?isDp=1&vid=",
-"https://vip.jiubojx.com/vip/?url=",
-"https://jx.parwix.com:4433/player/?url=",
-"https://bf.ssssu.cn/?url=",
-]
