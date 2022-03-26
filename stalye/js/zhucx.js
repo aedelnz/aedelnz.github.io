88fae23bd55afdc024027164fc3d5ec7a0e74393
aedelnz <aedelnz@qq.com>
2022-03-26 14:23:51 +0800

    00

--- stalye/js/zhucx.js
+++ stalye/js/zhucx.js
@@ -22,8 +22,6 @@ if (!!window.ActiveXObject || "ActiveXObject" in window) { //is IE?
 //}
 //Log
 
-console.log ("%c JIXIEJIDIGUAN"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em");
-console.log('%c 地址 http://jixiejidiguan.top','font-size:35px;color:#ff8400;');
 
 //Jquery
 document.write("<script src=\"https:\/\/cdn.bootcdn.net\/ajax\/libs\/jquery\/3.6.0\/jquery.min.js\"><\/script>");
@@ -38,75 +36,3 @@ document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=
 //document.write("<div class=\"deng-box\"><div class=\"deng\"><div class=\"xian\"><\/div><div class=\"deng-a\"><div class=\"deng-b\"><div class=\"deng-t\">节<\/div><\/div><\/div><div class=\"shui shui-a\"><div class=\"shui-c\"><\/div><div class=\"shui-b\"><\/div><\/div><\/div><\/div>");
 //document.write("<div class=\"deng-box1\"><div class=\"deng\"><div class=\"xian\"><\/div><div class=\"deng-a\"><div class=\"deng-b\"><div class=\"deng-t\">春<\/div><\/div><\/div><div class=\"shui shui-a\"><div class=\"shui-c\"><\/div><div class=\"shui-b\"><\/div><\/div><\/div><\/div>");
 
-
-(function() {
-if(document.getElementById('toTopLikeKuAn')){
-}else{
-function toTopLikeKuAn() {
-		var toTopBtn = document.createElement("div");
-		toTopBtn.id = "toTopBtn";
-		toTopBtn.setAttribute("style","font-size:4vw !important;width:10vw !important;height:10vw !important;line-height:10vw !important;text-align:center !important;background:url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAQAAABIkb+zAAABD0lEQVR42u3bMRLCMAwFUbz3v7MpKKACkvhLzsyqcCntO4DHfNx7eAgQIECAAAECBAgQIECAAAECBAgQIOBzxhzzxoBXfJ5AMr+CQDY/TyCdnyaQz88SqMhPEqjJzxGoyk8RyOXPMUeeQC7//SYJJPMrCGTz8wTS+WkC+fwsgYr8JIGa/ByBqvwUgbr8DIHK/ASB2vz1BKrzVxOoz19LoCN/JYGe/HUEuvJXEejLX0OgM38Fgd786wS6868S6M+/RmCH/CsE9sg/T2CX/LME9sk/R2Cn/DME/llSl3/8Or+X1OYfvc6vJfX5x67zfUlP/pHrwx8cAgQIECBAgAABAgQIECBAgAABAgQIECCgfJ7DZZC9LC8fDAAAAABJRU5ErkJggg==') no-repeat center center !important;background-size:3vw 3vw !important;background-color:rgba(250,250,250,0.9) !important;box-shadow:0px 1px 1px rgba(0,0,0,0.4);color:#000 !important;position:fixed !important;bottom:5vh !important;right:45vw !important;z-index:99999999999999999 !important;border-radius:100% !important;display:none;");
-		document.body.appendChild(toTopBtn);
-};
-function isScrollToTop() {
-	var toTopTimer;
-	var theBody = document.getElementsByTagName('body')[0];
-	var topTopBtn = document.getElementById('toTopBtn');
-	document.ontouchstart = function (e) {
-		if (toTopTimer) {
-			clearTimeout(toTopTimer);
-		};
-		mystartY = e.changedTouches[0].clientY;
-	};
-	document.ontouchmove = function (e) {
-		myendY = e.changedTouches[0].clientY;
-		var myY = myendY - mystartY;
-		if (myY > 0){
-			toTopBtn.style.opacity = "1";
-			toTopBtn.style.display = "block";
-			toTopBtn.style.transform = "rotateZ(0deg)";
-			toTopBtn.style.boxShadow = "0px 1px 1px rgba(0,0,0,0.4)";
-			toTopBtn.onclick = function () {
-				window.scrollTo(0,0);
-				this.style.display = "none";
-				toTopBtn.removeEventListener('click',this,false);
-			};
-		} else if (myY < 0) {
-			toTopBtn.style.opacity = "1";
-			toTopBtn.style.display = "block";
-			toTopBtn.style.transform = "rotateZ(180deg)";
-			toTopBtn.style.boxShadow = "0px -1px 1px rgba(0,0,0,0.4)";
-			toTopBtn.onclick = function () {
-				window.scrollTo(0, document.body.scrollHeight);	
-				this.style.display = "none";
-				toTopBtn.removeEventListener('click',this,false);
-			};
-		}else {
-			toTopBtn.style.display = "none";
-		};
-	};
-	document.ontouchend = function (e) {
-		toTopTimer = setTimeout(function () {
-			toTopBtn.style.transitionProperty="opacity,background-color";
-			toTopBtn.style.transitionDuration="500ms";
-			toTopBtn.style.transitionTimingFunction = "linear";
-			toTopBtn.style.opacity = "0";
-			toTopBtn.style.backgroundColor = "rgba(200,200,200,1)";
-			setTimeout(function() {
-				toTopBtn.style.display = "none";
-				toTopBtn.style.backgroundColor = "rgba(250,250,250,0.9)";
-			},500);
-		},3000);
-	};
-};
-var isHaveToTopBtn;
-isHaveToTopBtn = document.getElementById('toTopBtn');
-if (!isHaveToTopBtn) {
-	toTopLikeKuAn();
-	isScrollToTop();
-};
-var pans= document.createElement('b');		
-pans.id='toTopLikeKuAn';
-document.body.appendChild(pans);
-}
-})();
