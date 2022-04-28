e6e0141f08412d0d2486c29ccae19b308ffe1e60
aedelnz <44128582+aedelnz@users.noreply.github.com>
2022-04-28 21:08:32 +0800

    Add files via upload

--- stalye/js/zhucx.js
+++ stalye/js/zhucx.js
@@ -27,7 +27,5 @@ if (!!window.ActiveXObject || "ActiveXObject" in window) { //is IE?
 document.write("<script src=\"https:\/\/cdn.bootcdn.net\/ajax\/libs\/jquery\/3.6.0\/jquery.min.js\"><\/script>");
 
 
-//友盟
-document.write("<script type=\"text\/javascript\">document.write(unescape(\"%3Cspan id='cnzz_stat_icon_1279931368'%3E%3C\/span%3E%3Cscript src='https:\/\/s4.cnzz.com\/z_stat.php%3Fid%3D1279931368%26online%3D1%26show%3Dline' type='text\/javascript'%3E%3C\/script%3E\"));<\/script>");
 //我要拉
 document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"JMIJZqK4MvC0pOsF\",ck:\"JMIJZqK4MvC0pOsF\"});</script>")
