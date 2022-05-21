cf2885b65b4cc1f7ab9f41efd441a4c3836115b0
aedelnz <44128582+aedelnz@users.noreply.github.com>
2022-05-21 19:11:03 +0800

    Add files via upload

--- stalye/js/zhucx.js
+++ stalye/js/zhucx.js
@@ -4,26 +4,26 @@ document.write("<script src=\"https:\/\/jixiejidiguan.top\/stalye\/mdui\/js\/mdu
 document.write("<script src=\"https:\/\/jixiejidiguan.top\/stalye\/js\/jquery.min.js?v=1.0\"><\/script>");
 
 //获取浏览器页面可见高度和宽度
-var _PageHeight = document.documentElement.clientHeight,
-    _PageWidth = document.documentElement.clientWidth;
+//var _PageHeight = document.documentElement.clientHeight,
+//    _PageWidth = document.documentElement.clientWidth;
 //计算loading框距离顶部和左部的距离（loading框的宽度为215px，高度为61px）
-var _LoadingTop = _PageHeight > 61 ? (_PageHeight - 61) / 2 : 0,
-    _LoadingLeft = _PageWidth > 215 ? (_PageWidth - 215) / 2 : 0;
+//var _LoadingTop = _PageHeight > 61 ? (_PageHeight - 61) / 2 : 0,
+//    _LoadingLeft = _PageWidth > 215 ? (_PageWidth - 215) / 2 : 0;
 //加载gif地址
-var Loadimagerul="https://jixiejidiguan.top/stalye/img/加载.gif?v=1.0";
+//var Loadimagerul="https://jixiejidiguan.top/stalye/img/加载.gif?v=1.0";
 //在页面未加载完毕之前显示的loading Html自定义内容
-var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:100%;height:' + _PageHeight + 'px;top:0;background:#f3f8ff;opacity:1;filter:alpha(opacity=50);z-index:10000;"><div style="position: absolute; cursor1: wait; left: ' + _LoadingLeft + 'px; top:' + _LoadingTop + 'px; width:240px; height: 150px; background: #fff url('+Loadimagerul+') no-repeat; border: 1px solid #95B8E7;"></div></div>';
+//var _LoadingHtml = '<div id="loadingDiv" style="position:absolute;left:0;width:100%;height:' + _PageHeight + 'px;top:0;background:#f3f8ff;opacity:1;filter:alpha(opacity=50);z-index:10000;"><div style="position: absolute; cursor1: wait; left: ' + _LoadingLeft + 'px; top:' + _LoadingTop + 'px; width:240px; height: 150px; background: #fff url('+Loadimagerul+') no-repeat; border: 1px solid #95B8E7;"></div></div>';
 //呈现loading效果
-document.write(_LoadingHtml);
+//document.write(_LoadingHtml);
 //监听加载状态改变
-document.onreadystatechange = completeLoading;
+//document.onreadystatechange = completeLoading;
 //加载状态为complete时移除loading效果
-function completeLoading() {
-    if (document.readyState == "complete") {
-        var loadingMask = document.getElementById('loadingDiv');
-        loadingMask.parentNode.removeChild(loadingMask);
-    }
-}
+//function completeLoading() {
+//    if (document.readyState == "complete") {
+//        var loadingMask = document.getElementById('loadingDiv');
+//        loadingMask.parentNode.removeChild(loadingMask);
+//    }
+//}
 
 
 //Title
