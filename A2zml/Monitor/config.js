6ab0fe9130249635ab5ee1630dcfca503f900b8e
aedelnz <aedelnz@qq.com>
2023-10-02 21:16:07 +0800

    更新内容

--- A2zml/Monitor/config.js
+++ A2zml/Monitor/config.js
@@ -1,28 +1,20 @@
-// 配置
 window.Config = {
-  // 站点名
-  SiteName: '网站监控',
-  // 站点链接
-  SiteUrl: '/A2zml/Monitor',
-  // UptimeRobot Api Keys
-  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
-  ApiKeys: [
-    'ur1466828-96640b6a19c5204706e00c05',
-  ],
-  // 是否显示监测站点的链接
-  ShowLink: true,
-  // 日志天数
-  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
-  // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 30,
-  // 导航栏菜单
-  Navi: [
-    {
-      text: '原页',
-      url: 'https://stats.uptimerobot.com/0M2zqUkp83'
-    },{
-      text: '返回首页',
-      url: '/'
-    }
-  ]
+// 显示标题
+SiteName: '网站监控',
+// UptimeRobot Api Keys
+// 支持 Monitor-Specific 和 Read-Only
+ApiKeys: [
+'ur1466828-96640b6a19c5204706e00c05'
+],
+// 日志天数
+CountDays: 30,
+// 是否显示检测站点的链接
+ShowLink: true,
+// 导航栏菜单
+Navi: [
+{
+text: '主页',
+url: '/'
+}
+],
 };
