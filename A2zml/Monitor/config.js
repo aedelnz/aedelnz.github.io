bfd4aa4d5d3032d042e1edf4a0579082d90fa0e5
aedelnz <aedelnz@qq.com>
2024-01-09 16:48:27 +0800

    00

--- A2zml/Monitor/config.js
+++ A2zml/Monitor/config.js
@@ -1,20 +1,25 @@
 window.Config = {
-// 显示标题
-SiteName: '网站监控',
-// UptimeRobot Api Keys
-// 支持 Monitor-Specific 和 Read-Only
-ApiKeys: [
-'ur1466828-96640b6a19c5204706e00c05'
-],
-// 日志天数
-CountDays: 30,
-// 是否显示检测站点的链接
-ShowLink: true,
-// 导航栏菜单
-Navi: [
-{
-text: '主页',
-url: 'https://stats.uptimerobot.com/0M2zqUkp83'
-}
-],
+
+  // 显示标题
+  SiteName: '云端 站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1466828-96640b6a19c5204706e00c05'
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '查看原页',
+      url: 'https://stats.uptimerobot.com/0M2zqUkp83'
+    }
+  ],
 };
