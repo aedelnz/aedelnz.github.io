b1121062222690017f9bb0d04086f77799a03884
aedelnz <aedelnz@qq.com>
2025-02-27 11:18:27 +0800

    提交修改

--- ROOT/PORT/library/framework.js
+++ ROOT/PORT/library/framework.js
@@ -1,3 +1,5 @@
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"JMIJZqK4MvC0pOsF\",ck:\"JMIJZqK4MvC0pOsF\",autoTrack:true,hashMode:true,screenRecord:true});</script>");
+
 // 改进后的通用资源加载器
 async function loadResources(resources) {
   try {
@@ -62,3 +64,4 @@ const resources = [
 ];
 
 loadResources(resources);
+
