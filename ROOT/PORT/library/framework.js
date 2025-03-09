a67d9ea92e1f3c7ec9cee6208f338c9b71d04653
麗姫を描く <aedelnz@qq.com>
2025-03-09 09:15:01 +0800

    js

--- ROOT/PORT/library/framework.js
+++ ROOT/PORT/library/framework.js
@@ -1,5 +1,22 @@
 document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"JMIJZqK4MvC0pOsF\",ck:\"JMIJZqK4MvC0pOsF\",autoTrack:true,hashMode:true,screenRecord:true});</script>");
 
+const scriptElement = document.querySelector('script#JIXIEJIDIGUAN');
+let Events = '';
+if (scriptElement) {
+    const srcValue = scriptElement.getAttribute('src');
+    const hrefMatch = srcValue.match(/[?&]href=([^&]+)/);
+    Events = hrefMatch ? decodeURIComponent(hrefMatch[1]) : '';
+}
+
+const resources = [
+  { type: 'css', url: Events + '/ROOT/PORT/library/mdui.css' },
+  { type: 'js', url: Events + '/ROOT/PORT/library/mdui.global.js' },
+  { type: 'css', url: Events + '/ROOT/PORT/library/normalize.min.css' }
+];
+
+loadResources(resources);
+
+
 // 改进后的通用资源加载器
 async function loadResources(resources) {
   try {
@@ -56,12 +73,4 @@ function loadJS(url) {
   });
 }
 
-// 使用示例
-const resources = [
-  { type: 'css', url: 'https://jixiejidiguan.top/ROOT/PORT/library/mdui.css' },
-  { type: 'js', url: 'https://jixiejidiguan.top/ROOT/PORT/library/mdui.global.js' },
-  { type: 'css', url: 'https://jixiejidiguan.top/ROOT/PORT/library/normalize.min.css' }
-];
-
-loadResources(resources);
 
