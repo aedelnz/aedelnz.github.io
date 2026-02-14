350c79bb9c776fe8568c1c6c456b1c134d760ed5
aedelnz <aedelnz@qq.com>
2026-02-14 23:35:29 +0800

    ++

--- A2zml/v2/App.tsx
+++ A2zml/v2/App.tsx
@@ -108,7 +108,7 @@ function App() {
           </Layout.Content>
 
           <Layout.Footer className="footer">
-            <Divider style={{ margin: '0' }} />
+            <Divider className="divider" />
             <div style={{ padding: '1.25rem', textAlign: 'center' }}>
               <Typography.Paragraph bold>Copyright © 2020 - {currentYear}  <Link hoverable={false} href='https://jixiejidiguan.top'>画的个人记录</Link>. All Rights Reserved.</Typography.Paragraph>
               <Typography.Text>本网站提供的内容信息仅供参考，用户应自行判断并承担使用风险。</Typography.Text>
