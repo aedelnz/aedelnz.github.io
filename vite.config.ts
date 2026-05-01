77c1cff10b4ff9411ba869ae9ac333633db79364
aedelnz <aedelnz@qq.com>
2026-05-01 20:59:06 +0800

    111

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   base: '/',
    build: {
-    outDir: 'dist',
+    outDir: 'docs',
     rolldownOptions: {
       input: {
         main: resolve(import.meta.dirname, 'index.html'),
