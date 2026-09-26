40cc950eebaecc04eb4e964cf6cff49c5c133dfc
麗姫を描く <aedelnz@qq.com>
2026-09-26 20:40:18 +0800

    Update main.tsx

--- src/a2zml/v2/main.tsx
+++ src/a2zml/v2/main.tsx
@@ -6,9 +6,9 @@ import { Layout, List, Nav, Typography } from '@douyinfe/semi-ui'
 import { OpenTool, SubscribeNode } from './Data'
 import DarkMode from '../../component/fast/DarkMode'
 import Hitokotos from '../../component/Hitokotos'
+import Footers from '../../component/Footers'
 import OpenToolCard from './component/OpenToolCard'
 import SubscribeNodeCard from './component/SubscribeNodeCard'
-import Footers from '../../component/Footers'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -32,26 +32,20 @@ createRoot(document.getElementById('root')!).render(
                   grid={{ gutter: 12, xs: 24, sm: 12, md: 12, lg: 8, xl: 8, xxl: 6, }}
                   dataSource={OpenTool}
                   renderItem={item => (
-                    <List.Item style={{ margin: '8px 2px' }}>
-                      <OpenToolCard data={item} />
-                    </List.Item>
+                    <List.Item style={{ margin: '8px 2px' }}><OpenToolCard data={item} /></List.Item>
                   )}
                 />
               </div>
             </section>
             <section style={{ margin: '16px 0' }}>
               <Typography.Title heading={3}>订阅节点</Typography.Title>
-              <Typography.Paragraph>
-                来源 互联网 收录
-              </Typography.Paragraph>
+              <Typography.Paragraph>来源 互联网 收录</Typography.Paragraph>
               <div style={{ margin: '12px 0' }}>
                 <List
                   grid={{ gutter: 12, xs: 24, sm: 12, md: 12, lg: 8, xl: 8, xxl: 6, }}
                   dataSource={SubscribeNode}
                   renderItem={item => (
-                    <List.Item style={{ margin: '8px 2px' }}>
-                      <SubscribeNodeCard data={item} />
-                    </List.Item>
+                    <List.Item style={{ margin: '8px 2px' }}><SubscribeNodeCard data={item} /></List.Item>
                   )}
                 />
               </div>
